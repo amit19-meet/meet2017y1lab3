1c2aeb1c36cb5df0bd424109a06bf1a1a76912f0
amit19-meet <amit19@meet.mit.edu>
2017-07-25 12:22:18 +0300

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print("hello world is how you say hello world in english")
+print("Γειά σου Κόσμε is how you say hello world in Greek")
+print("Olá Mundo is how you say hello world in Portuguese")
+print("Ahoj světe is how you say hello world in Czech")
+print("안녕 세상 is how you say hello world in Korean")
+print("你好，世界 is how you say hello world in Chinese")
+print("नमस्ते दुनिया is how you say hello world in Hindi")
+print("Salamu, Dunia is how you say hello world in Swahili")
+
+
+
+
+
+
